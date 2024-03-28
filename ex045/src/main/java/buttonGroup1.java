ab5134d224a1b5c66cb0b5203618992609399337
Joaquim Angelo <156119400+Joaquim-angelo@users.noreply.github.com>
2024-03-28 16:04:35 -0300

    atualizando....

--- ex045/src/main/java/buttonGroup1.java
+++ ex045/src/main/java/buttonGroup1.java
@@ -1,16 +1,12 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package ex044;
 
 /**
  *
  * @author Joaquim
  */
-public class Ex044 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+class buttonGroup1 {
+    
 }
